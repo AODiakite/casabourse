484cdf1d8bc92fd5b832c82f33fd159d13697cd7
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2021-12-14 17:11:58 +0100

    update

--- R/install_pymodules.R
+++ R/install_pymodules.R
@@ -2,6 +2,7 @@
 #' @description For a first use of the package, you must perform this function that will automatically download all the dependencies you need
 #' @export
 install.tools=function(){
+  reticulate::repl_python()
   reticulate::py_install("pandas")
   reticulate::py_install("bs4")
   reticulate::py_install("requests")
