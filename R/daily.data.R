a889f7f237d1aa85a90389206c005a28ea02f07e
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2022-03-14 22:43:57 +0100

    v1.3.0

--- R/daily.data.R
+++ R/daily.data.R
@@ -18,7 +18,8 @@ daily.data <- function(ticker = "MASI", from = "28-12-2020", to = "28-02-2022") 
           from <- as.Date(from, "%d-%m-%Y")
           to <- as.Date(to, "%d-%m-%Y")
           url <- "https://www.leboursier.ma/api?method=getMasiHistory&periode=5y&format=json"
-          masi <- RJSONIO::fromJSON(url)
+          masi <- httr::GET(url)
+          masi <- httr::content(masi, encoding = "UTF-8")
           masi <- masi$result
           Date <- unlist(masi$labels)
           Date <- as.Date.POSIXct(Date)
@@ -48,7 +49,8 @@ daily.data <- function(ticker = "MASI", from = "28-12-2020", to = "28-02-2022") 
           link <- paste("https://www.leboursier.ma/api?method=getPriceHistory&ISIN=", "&format=json&from=", sep = toString(isin))
           link <- paste(link, "&to=", sep = toString(from))
           link <- paste0(link, toString(to))
-          r <- RJSONIO::fromJSON(link)
+          r <- httr::GET(link)
+          r <- httr::content(r, encoding = "UTF8-SIG")
           df <- r$result
           Date <- rep(list(NULL), length(df))
           Value <- rep(list(NULL), length(df))
@@ -85,7 +87,8 @@ daily.data <- function(ticker = "MASI", from = "28-12-2020", to = "28-02-2022") 
         }
       )
     }
-  } else {
+  }
+  else {
     print("Wrong ticker! You can display all tickers with tickers() function")
   }
 }
--- cran-comments.md
+++ cran-comments.md
@@ -1,5 +1,8 @@
 ## R CMD check results
 
+#Version: 1.2.0
+This new update comes to fix some warning messages. Indeed, several functions display warnings during execution, so I had to look for another alternative to read the json files which were the cause of the warnings
+
 0 errors | 0 warnings | 1 note
 
 * This is a new release.
